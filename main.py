56f64cfdba9589b533d12bf945626ede9fa08399
Sara Martín <sara.04.sm@gmail.com>
2024-11-25 18:31:32 +0100

    added train, test, metrices and storage of results

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from torchvision import transforms
 import pandas as pd
 
 from utils.dataloader import Flickr30kDataset
+from utils.train_test import initialize_storage, train_model, test_model
 
 # GLOBAL VARIABLES
 IMAGES_DIR = './data/images/'
 CAPTIONS_DIR = './data/captions.csv'
 TRAIN_SIZE, TEST_SIZE, VAL_SIZE = 0.8, 0.1, 0.1
 
+initialize_storage()
 
+# Data transfpormation
 transform = transforms.Compose([transforms.Resize((224, 224)),
                                 transforms.ToTensor()])
 
